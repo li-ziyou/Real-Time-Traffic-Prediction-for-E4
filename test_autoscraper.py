0a7694c8b822b136ffd8c266df4ebd91508ff0b8
Tilosmsh <lzy6078@hotmail.com>
2023-01-04 21:32:45 +0100

    del autoscraper

--- test_autoscraper.py
+++
@@ -1,6 +0,0 @@
-from autoscraper import AutoScraper
-url = 'https://www.tomtom.com/traffic-index/stockholm-traffic/'
-wanted_list = ['Wednesday, 28 Dec 2022', '8:00 PM', '12%']
-scraper = AutoScraper()
-res = scraper.build(url, wanted_list)
-print(res)
