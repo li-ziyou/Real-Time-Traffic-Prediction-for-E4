4689574a869c6b0216f4ced99e1236363878fbad
Chenzhou <171994636@qq.com>
2023-01-10 22:01:38 +0100

    local training finished

--- Training.py
+++ Training.py
@@ -1,9 +1,5 @@
-from Supervised import LazyRegressor
-from sklearn import datasets
-from sklearn.utils import shuffle
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
@@ -21,44 +17,48 @@ features = traffic.remove_columns(["congestionLevel"]) # features
 target = traffic.remove_columns(['referenceTime', 't', 'ws', 'prec1h', 'fesn1h', 'vis', 'confidence']) #lable
 print(features, "\n" ,target)
 
-
-#X, y = shuffle(features.data, target.data, random_state=13)
-#X, y = features.data, target.data
-#X = X.astype(np.float32)
-#offset = int(X.shape[0] * 0.9)
-
-
 X_train_df = pd.DataFrame.from_dict(features["train"])
 y_train_df = pd.DataFrame.from_dict(target["train"])
 X_test_df  = pd.DataFrame.from_dict(features["test"])
 y_test_df  = pd.DataFrame.from_dict(target["test"])
-#print(X_train_df,y_train_df)
 
 X_train, y_train = X_train_df[[ 't', 'ws', 'prec1h', 'fesn1h', 'vis', 'confidence']], y_train_df['congestionLevel'] #ref time
 X_test, y_test = X_test_df[[ 't', 'ws', 'prec1h', 'fesn1h', 'vis', 'confidence']], y_test_df['congestionLevel']
-#print(y_train)
 
-reg = LazyRegressor(verbose=0, ignore_warnings=False, custom_metric=None)
-models, predictions = reg.fit(X_train, X_test, y_train, y_test)
-print(models)
+# Multi regression
+#reg = LazyRegressor(verbose=0, ignore_warnings=False, custom_metric=None)
+#models, predictions = reg.fit(X_train, X_test, y_train, y_test)
+#print(models)
 
-print(X_train,"\n y train:\n",y_train)
-model = LinearRegression()#copy_X=True, fit_intercept=True, n_jobs=1, normalize=False)
+# Single reg
+model = LinearRegression()
 model.fit(X_train,y_train)
 
 predictions = model.predict(X_test)
+
 plt.scatter(y_test,predictions)
 plt.xlabel('Y Test')
 plt.ylabel('Predicted Y')
 
+from sklearn import metrics
+print('MAE:', metrics.mean_absolute_error(y_test, predictions))
+print('MSE:', metrics.mean_squared_error(y_test, predictions))
+print('RMSE:', np.sqrt(metrics.mean_squared_error(y_test, predictions)))
+
+
+# Save the model to huggingface
+import pickle
+filename = 'traffic_model.pkl'
+pickle.dump(model, open(filename, 'wb'))
+
+from huggingface_hub import create_repo
+create_repo(repo_id="Traffic_Prediction")
+
+from huggingface_hub import upload_file
+upload_file(
+   path_or_fileobj="traffic_model.pkl",
+   path_in_repo="traffic_model.pkl",
+   repo_id="tilos/Traffic_Prediction"
+)
 
 print("done")
-
-kwargs = {
-    "dataset_tags": "Traffic data",
-    "dataset": "Traffic and weather",  # a 'pretty' name for the training dataset
-    "model_name": "Traffic Prediction",  # a 'pretty' name for our model
-    "tasks": "Traffic Regression",
-    "tags": "regression",
-    }
-model.push_to_hub(**kwargs)
