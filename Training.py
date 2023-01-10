5b92fda475a761c14dc7211a5240c0b402887dda
Chenzhou <171994636@qq.com>
2023-01-10 20:30:31 +0100

    v2

--- Training.py
+++ Training.py
@@ -3,6 +3,7 @@ from sklearn import datasets
 from sklearn.utils import shuffle
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
 from huggingface_hub import login, notebook_login
 
 login(token="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")
@@ -13,18 +14,19 @@ traffic_dataset = DatasetDict()
 traffic_dataset = load_dataset("tilos/IL2223_project") #read dataset from huggingface
 #print(traffic_dataset)
 traffic = traffic_dataset['train'].train_test_split(test_size=0.2, shuffle=True) #splite train and test
-print(traffic)
 
 target = traffic.remove_columns(['referenceTime', 't', 'ws', 'prec1h', 'fesn1h', 'vis', 'confidence', 'congestionLevel']) #lable
 features = traffic.remove_columns(["congestionLevel"]) # features
+print(features, "\n",target)
 
-X, y = shuffle(features.data, target.data, random_state=13)
-X = X.astype(np.float32)
 
-offset = int(X.shape[0] * 0.9)
+#X, y = shuffle(features.data, target.data, random_state=13)
+#X, y = features.data, target.data
+#X = X.astype(np.float32)
+#offset = int(X.shape[0] * 0.9)
 
-X_train, y_train = X[:offset], y[:offset]
-X_test, y_test = X[offset:], y[offset:]
+X_train, y_train = features["train"].data, target["train"].data
+X_test, y_test = features["test"].data, target["test"].data
 
 reg = LazyRegressor(verbose=0, ignore_warnings=False, custom_metric=None)
 models, predictions = reg.fit(X_train, X_test, y_train, y_test)
